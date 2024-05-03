18e4a0bcc2daf5c8f58bde2d1bb4bcbc13e33b23
pavi6804 <125538527+pavi6804@users.noreply.github.com>
2024-05-03 15:02:02 +0530

    image-changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ const App = () => {
       <main className="p-3">
         <div className="row">
           <div className="col-4">
-            <MovieCard title="Ghilli - (2004)" image="https://pbs.twimg.com/media/EVyhuIsU4AANZ1B.jpg:large" desc="Velu, an aspiring Kabaddi player, is in Madurai to participate in one of the regional matches when he rescues Dhanalakshmi from Muthupandi, a powerful man keen on marrying the girl against her wishes." />
+            <MovieCard title="Ghilli - (2004)" image="https://assets-in.bmscdn.com/discovery-catalog/events/et00000976-mcbzcuvucn-landscape.jpg" desc="Velu, an aspiring Kabaddi player, is in Madurai to participate in one of the regional matches when he rescues Dhanalakshmi from Muthupandi, a powerful man keen on marrying the girl against her wishes." />
           </div>
           <div className="col-4">
-            <MovieCard title="Dheena - (2001)" image="https://pbs.twimg.com/media/GARVvKnXMAETr-v.jpg:large" desc="Dheena, a gangster's confidante, learns that his brother has fallen for the gangster's sister. However, her accidental death leads to a precarious situation after the gangster vows to kill her lover." />
+            <MovieCard title="Remo - (2016)" image="https://d2h7z5r5pp4sed.cloudfront.net/tamil/news/remo-firstlook23616-m.jpg" desc="SK, an aspiring actor, falls in love with Kavya, a doctor. However, she is engaged to another man and SK tries to sabotage their relationship by making her fall for him." />
           </div>
           <div className="col-4">
             <MovieCard title="Mankatha - (2011)" image="https://m.media-amazon.com/images/M/MV5BZjEwYjkxZGEtMjZlMy00MDZhLTk0MWYtNDY0NTZiNmZkM2Q4XkEyXkFqcGdeQXVyMTEzNzg0Mjkx._V1_.jpg" desc="Inspector Vinayak is suspended after he lets a smuggler escape. He then meets a group of men who plan to whisk away 500 crore belonging to a gangster. He promises to help them for a share in the loot." />
